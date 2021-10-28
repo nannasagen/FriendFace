c59a654bd3434412e672d6e50201458fe4003008
nannasagen <82771340+nannasagen@users.noreply.github.com>
2021-10-28 15:06:07 +0200

    Add files via upload

---
+++ Friends.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendFace
+{
+    class Friends
+    {
+        public string UserName;
+        public string UserQuote;
+        public string UserPassword;
+        public bool IsLoggedIn;
+        public int UserAge;
+        public string UserInfo;
+
+        List<Friends> friendsList = new List<Friends>();
+
+        public Friends(string username, string userquote, string userpassword)
+        {
+            UserName = username;
+            UserQuote = userquote;
+            UserPassword = userpassword;
+            UserInfo = "Name " + UserName + "Quote: "+ UserQuote +" Alder: "+ UserAge;
+        }
+
+        public void AddFriend(Friends friend)
+        {
+            //return UserInfo; //Push til en venne-liste.
+            friendsList.Add(friend);
+            Console.WriteLine("You added a new friend :-) " + friend.UserName );
+        }
+
+        public void RemoveFriend(Friends friend)
+        {
+            if(friendsList.Contains(friend)) friendsList.Remove(friend);
+            Console.WriteLine("You removed a friend :-( " + friend.UserName );
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FriendFace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Friends> UserList = new List<Friends>();
+
+            UserList.Add(new Friends("Nanna", "Tomater ruler!", "Passord"));
+            UserList.Add(new Friends("Tomato", "Ketchup is genocide", "CatchUp"));
+            UserList.Add(new Friends("Orange", "You peel me?", "OrangeYouGlad"));
+            UserList.Add(new Friends("Cake", "Eat me!", "Cheese"));
+
+            Friends myuser = UserList[0];
+
+            Console.WriteLine("Enter FriendName:");
+            string inputs = Console.ReadLine();
+            foreach (Friends userfriend in UserList)
+            {
+                if(userfriend.UserName == inputs)
+                    myuser.AddFriend(userfriend);
+            }
+
+            Console.WriteLine("What friends do you want to YEEEEET? ");
+            inputs = Console.ReadLine();
+            foreach (Friends myfriends in UserList)
+            {
+                if(myfriends.UserName == inputs)
+                {
+                    myuser.RemoveFriend(myfriends);
+
+                }
+            }
+        }
+    }
+}
